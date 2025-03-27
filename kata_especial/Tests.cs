1d63713354ee1bf68144ed739261fc3ef058790f
Diego Ruiz Gil <80207565+DiegoRuizGil@users.noreply.github.com>
2025-03-27 12:54:12 +0100

    feat: exempt minimum

--- kata_especial/Tests.cs
+++ kata_especial/Tests.cs
@@ -1,10 +1,23 @@
+using NUnit.Framework.Constraints;
+
 namespace kata_especial;
 
 public class Tests
 {
+    /*
+     * IRPF -> impuesto a todas las personas físicas
+     * mínimo exento -> 15.876 --> 0% IRPF
+     * 8.701
+     */
+    
     [Test]
-    public void Test1()
+    public void ExemptMin()
     {
-        Assert.Pass();
+        Assert.That(IRPF(8701), Is.EqualTo(0));
+    }
+
+    private float IRPF(float income)
+    {
+        return 0;
     }
 }
