8420c8dd50cc002485c76b3276d941bae937bcdb
Diego Ruiz Gil <80207565+DiegoRuizGil@users.noreply.github.com>
2025-03-30 17:02:06 +0200

    refactor: extract TaxBracket struct

--- kata_especial/IRPF.cs
+++ kata_especial/IRPF.cs
@@ -11,24 +11,9 @@ public static class IRPF
      *
      * más ingresos -> más beneficios
      *
-     * tramo 1 -> [0, 12450] -> 19%
-     * tramo 2 -> (12450, 20200] -> 24%
-     * tramo 3 -> (20200, 35200] -> 30%
-     * tramo 4 -> (35200, 60000] -> 37%
-     * tramo 5 -> (60000, 300000] -> 45%
-     * tramo 6 -> (300000, infinito) -> 47%
-     *
-     * mínimo exento = 15876
-     *
      * cifras MUY decimales
      * incluir gastos para los autónomos (solo)
      */
-
-    private const float ExemptMinThreshold = 15876;
-    private const float SecondThreshold = 20200;
-    private const float ThirdThreshold = 35200;
-    private const float FourthThreshold = 60000;
-    private const float FifthThreshold = 300000;
     
     public static float Of(float income)
     {
@@ -36,26 +21,26 @@ public static class IRPF
 
         var irpf = 0f;
 
-        if (income > ExemptMinThreshold)
-            irpf += IncomeBracketIRPF(income, ExemptMinThreshold, SecondThreshold, 0.24f);
-        if (income > SecondThreshold)
-            irpf += IncomeBracketIRPF(income, SecondThreshold, ThirdThreshold, 0.30f);
-        if (income > ThirdThreshold)
-            irpf += IncomeBracketIRPF(income, ThirdThreshold, FourthThreshold, 0.37f);
-        if (income > FourthThreshold)
-            irpf += IncomeBracketIRPF(income, FourthThreshold, FifthThreshold, 0.45f);
-        if (income > FifthThreshold)
-            irpf += (income - FifthThreshold) * 0.47f;
+        if (income > TaxBracket.ExemptMinThreshold)
+            irpf += IncomeBracketIRPF(income, TaxBracket.Second());
+        if (income > TaxBracket.SecondThreshold)
+            irpf += IncomeBracketIRPF(income, TaxBracket.Third());
+        if (income > TaxBracket.ThirdThreshold)
+            irpf += IncomeBracketIRPF(income, TaxBracket.Fourth());
+        if (income > TaxBracket.FourthThreshold)
+            irpf += IncomeBracketIRPF(income, TaxBracket.Fifth());
+        if (income > TaxBracket.FifthThreshold)
+            irpf += (income - TaxBracket.FifthThreshold) * TaxBracket.Sixth().Percent;
         
         return irpf;
     }
 
-    private static float IncomeBracketIRPF(float income, float lowerThreshold, float upperThreshold, float percent)
+    private static float IncomeBracketIRPF(float income, TaxBracket bracket)
     {
-        float amountExceeded = income - upperThreshold;
-        float thresholdIncome = income - lowerThreshold;
+        float amountExceeded = income - bracket.UpperThreshold;
+        float thresholdIncome = income - bracket.LowerThreshold;
         if (amountExceeded > 0)
             thresholdIncome -= amountExceeded;
-        return thresholdIncome * percent;
+        return thresholdIncome * bracket.Percent;
     }
 }
---
+++ kata_especial/TaxBracket.cs
@@ -0,0 +1,50 @@
+﻿namespace kata_especial;
+
+public readonly struct TaxBracket
+{
+    private readonly float _lowerThreshold;
+    private readonly float _upperThreshold;
+    private readonly float _percent;
+    
+    public float LowerThreshold => _lowerThreshold;
+    public float UpperThreshold => _upperThreshold;
+    public float Percent => _percent;
+    
+    public const float ExemptMinThreshold = 15876;
+    public const float SecondThreshold = 20200;
+    public const float ThirdThreshold = 35200;
+    public const float FourthThreshold = 60000;
+    public const float FifthThreshold = 300000;
+
+    private TaxBracket(float lowerThreshold, float upperThreshold, float percent)
+    {
+        _lowerThreshold = lowerThreshold;
+        _upperThreshold = upperThreshold;
+        _percent = percent;
+    }
+
+    public static TaxBracket Second()
+    {
+        return new TaxBracket(ExemptMinThreshold, SecondThreshold, 0.24f);
+    }
+
+    public static TaxBracket Third()
+    {
+        return new TaxBracket(SecondThreshold, ThirdThreshold, 0.30f);
+    }
+
+    public static TaxBracket Fourth()
+    {
+        return new TaxBracket(ThirdThreshold, FourthThreshold, 0.37f);
+    }
+
+    public static TaxBracket Fifth()
+    {
+        return new TaxBracket(FourthThreshold, FifthThreshold, 0.45f);
+    }
+
+    public static TaxBracket Sixth()
+    {
+        return new TaxBracket(FifthThreshold, -1f, 0.47f);
+    }
+}
