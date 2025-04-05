c2965e03008f1e64a62c39ad13e153c7ac81bf57
Diego Ruiz Gil <80207565+DiegoRuizGil@users.noreply.github.com>
2025-04-05 16:21:42 +0200

    feature: obtain annual salary

--- kata_especial/PaySheetTests.cs
+++ kata_especial/PaySheetTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using NUnit.Framework.Constraints;
 
 namespace kata_especial;
 
@@ -7,7 +8,7 @@ public class PaySheetTests
     /*
      * nomina:
      *  - (derivado) sueldo neto del trabajador (employee's net salary)
-     *  - porcentaje IRPF deducido (15%)
+     *  - porcentaje IRPF deducido (15%) (withholding tax rate)
      *  - aportacion empresa -> bruto + seguridad social (company's contribution)
      *  - sueldo bruto trabajador (employee's gross salary)
      *
@@ -29,7 +30,18 @@ public class PaySheetTests
         Assert.That(NetSalary(1800.00f, 0.15f), Is.EqualTo(1530.00f));
     }
 
-    private float NetSalary(float grossSalary, float withholdingTaxRate)
+    [Test]
+    public void AnnualSalaryWith12Payments()
+    {
+        Assert.That(AnnualSalary(1800.00f, 0.15f, 12), Is.EqualTo(18360.00f));
+    }
+
+    private static float AnnualSalary(float grossSalary, float withholdingTaxRate, int payments)
+    {
+        return NetSalary(grossSalary, withholdingTaxRate) * payments;
+    }
+
+    private static float NetSalary(float grossSalary, float withholdingTaxRate)
     {
         Debug.Assert(withholdingTaxRate >= 0);
         Debug.Assert(withholdingTaxRate <= 1);
@@ -37,4 +49,6 @@ public class PaySheetTests
         
         return grossSalary * (1 - withholdingTaxRate);
     }
+    
+    
 }
